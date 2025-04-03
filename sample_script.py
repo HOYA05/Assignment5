b0b86ba80e8c36ef1e3bc85de4de057c358369f7
HOYA05 <jsn564040@gmail.com>
2025-04-03 10:17:24 +0000

    sample_script.py in branch01

--- sample_script.py
+++ sample_script.py
@@ -1,2 +1,9 @@
 
-# 커밋테스트용 주석
+def square(n):
+    """주어진 숫자의 제곱을 반환합니다."""
+    return n * n
+
+if __name__ == "__main__":
+    number = 5
+    result = square(number)
+    print(f"{number}의 제곱은 {result}입니다.")
